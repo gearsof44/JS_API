4efa5143f559aaa1bd67fd5b01df4a0da859eb54
gearsof44 <gearsof44officiel@gmail.com>
2017-10-17 17:31:37 +0200

    changes version near good
    changes version near good

--- src/application.js
+++ src/application.js
@@ -1,37 +1,70 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var UserForm = require("./components/UserForm");
-var RecoverMessages = require("./components/RecoverMessages");
-var DeleteMessage = require("./components/DeleteMessage");
-var SendMessage = require("./components/SendMessage");
+var SignIn = require("./components/SignIn");
+var RecupMessage = require("./components/RecupMessage");
+var EnvoiMessage = require("./components/EnvoiMessage");
+var SupMessage = require("./components/SupMessage");
+var render  = require("react-dom").default;
+var AsyncButton = require("react-async-button").default;
 
-class MessyTp extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          currentHours : new Date().getHours(),
-          currentMinutes : ( new Date().getMinutes() < 10 ? "0" : "" ) + new Date().getMinutes(),
-          currentSeconds : ( new Date().getSeconds() < 10 ? "0" : "" ) + new Date().getSeconds()
-        };
-        this.incrementTime = this.incrementTime.bind(this);
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idUser: sessionStorage.getItem('idUser'),
+      token: sessionStorage.getItem('token')
     }
+    this.registerConnection = this.registerConnection.bind(this);
+    this.disconnect = this.disconnect.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
+  }
 
-    incrementTime() {
-        this.setState({ currentHours: new Date().getHours() }, { currentMinutes: new Date().getHours() }, { currentSeconds: new Date().getHours() });
-    }
+  disconnect(event){
+    sessionStorage.clear();
+    console.log(sessionStorage);
+  }
 
-    render() {
-        return (
-            <span>
-              { this.state.currentHours } : { this.state.currentMinutes } : { this.state.currentSeconds }
-            </span>
-        );
-    }
+  registerConnection(user) {
+    console.log(user);
+    sessionStorage.setItem('token', user.token);
+    sessionStorage.setItem('idUser', user.user.id);
+    this.setState({idUser: user.user.id})
+    this.setState({token: user.token})
 
-    componentDidMount() {
-      setInterval(() => this.setState({ currentHours: new Date().getHours() }, { currentMinutes: new Date().getHours() }, { currentSeconds: new Date().getHours() }), 1000 )
+  }
+
+   clickHandler(event) {
+    return new Promise((resolve, reject) => {
+      // some async stuff
+      setTimeout(resolve, 500);
+      this.disconnect(event);
+
+    })
+  }
+
+  render() {
+    if(!this.state.token){
+      return (
+        <div id="Connect">
+          <UserForm onUserCreated={this.registerConnection}/>
+          <SignIn onUserLogged={this.registerConnection}/>
+        </div>
+        )
     }
+    else {
+      return (
+        <div>
+          <RecupMessage idUser={this.state.idUser} token={this.state.token}/>
+          <EnvoiMessage token={this.state.token}/>
+          <form onSubmit={this.disconnect}>
+            <input className ="btn btn-primary" type="submit" value="Se déconnecter" />
+          </form>
+        </div>
+      )
+    }
+    return <div>rien</div>
+  }
 }
 
-ReactDOM.render(<ClickableTitle title="Hello World"/>, document.getElementById("main"));
+ReactDOM.render(<Application />, document.getElementById("main"));
